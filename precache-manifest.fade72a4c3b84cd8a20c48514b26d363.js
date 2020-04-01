fe18edc626b4b9970781e2a57c36a3743204c140
papstchaka <alex.christoph.ac@gmail.com>
2020-04-01 14:29:32 +0200

    Updates

---
+++ precache-manifest.fade72a4c3b84cd8a20c48514b26d363.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7327ddea5289f31b197ac42383c56db4",
+    "url": "/smartbrain/index.html"
+  },
+  {
+    "revision": "c463d29a8f6b6a7671d6",
+    "url": "/smartbrain/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "0848d8b827ef3a86e85c",
+    "url": "/smartbrain/static/css/main.1dbce5ad.chunk.css"
+  },
+  {
+    "revision": "c463d29a8f6b6a7671d6",
+    "url": "/smartbrain/static/js/2.101270dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/smartbrain/static/js/2.101270dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0848d8b827ef3a86e85c",
+    "url": "/smartbrain/static/js/main.fb0b494c.chunk.js"
+  },
+  {
+    "revision": "b45c648726b4854f65d7",
+    "url": "/smartbrain/static/js/runtime-main.003b82b7.js"
+  }
+]);
