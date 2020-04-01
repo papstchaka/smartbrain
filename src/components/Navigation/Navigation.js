ff498ad2fc61cc39fed545b064a1c7ffde522a30
papstchaka <alex.christoph.ac@gmail.com>
2020-04-01 09:57:40 +0200

    finished backend + database / deployment to do

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,7 +4,7 @@ function Navigation( {onRouteChange, isSignedIn} ) {
     if (isSignedIn) {
         return (
             <nav style={{display: "flex", justifyContent: 'flex-end'}}>
-                <p onClick={() => onRouteChange('signout')} className='f3 link dim black underline pa3 pointer'>
+                <p onClick={() => onRouteChange('signin')} className='f3 link dim black underline pa3 pointer'>
                     Sign Out
                 </p>
             </nav>
