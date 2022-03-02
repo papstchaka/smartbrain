8e0a61c66282895176df9eeaa2ae1d27f1933ed6
papstchaka <alex.christoph.ac@gmail.com>
2022-03-02 16:13:35 +0100

    blurred background / increased text size

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -11,12 +11,12 @@ function ImageLinkForm({ onInputChange, onPictureSubmit }) {
     
     return (
         <div>
-            <p className="f3">
+            <p className="f2 b">
                 {'This Magic Brain will detect faces in your pictures. Give it a try!'}
             </p>
             <div className="center">
                 <div className="form center pa4 br3 shadow-5">
-                    <input type="text" className="f4 pa2 w-70 center" onChange={onInputChange} onKeyPress={handleKeyPress}/>
+                    <input type="text" className="f4 pa2 w-70 center backgroundwhite" onChange={onInputChange} onKeyPress={handleKeyPress}/>
                     <button 
                         className="w-30 grow f4 link ph3 pv2 dib white bg-gray"
                         onClick={onPictureSubmit}>
