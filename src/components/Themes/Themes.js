e4399db90fe7f0626570d3faf51dd984181cf28b
papstchaka <alex.christoph.ac@gmail.com>
2022-03-02 16:13:54 +0100

    added dark mode

---
+++ src/components/Themes/Themes.js
@@ -0,0 +1,20 @@
+export const lightTheme = {
+    primary: "#2e93aa",
+    secondary: "#00cdac",
+    tertiary: "#ced6d9",
+    modal1: "white",
+    modal2: "white",
+    body: '#FFF',
+    text: '#363537',
+    shadow: "#d3d3d3",
+}
+export const darkTheme = {
+    primary: "rgba(46, 147, 170, 0.5)",
+    secondary: "rgba(0, 205, 172, 0.5)",
+    tertiary: "rgba(206, 214, 217, 0.5)",
+    modal1: "rgba(0, 0, 0, 0.5)",
+    modal2: "rgba(0, 0, 0, 0.5)",
+    body: '#363537',
+    text: '#FFF',
+    shadow: "#484848",
+}
---
+++ src/components/Themes/globalStyles.js
@@ -0,0 +1,38 @@
+import styled, { createGlobalStyle } from "styled-components"
+export const GlobalStyles = createGlobalStyle`
+  body {
+    background: ${({ theme }) => theme.body};
+    background-image: linear-gradient(89deg, ${({ theme }) => theme.primary} 0%, ${({ theme }) => theme.secondary} 100%);
+    color: ${({ theme }) => theme.text};
+  }
+
+  & modal-big {
+    background-image: linear-gradient(89deg, ${({ theme }) => theme.modal1} 0%, ${({ theme }) => theme.modal2} 100%);
+  }
+
+  & .cta-btn--hero {
+    background: linear-gradient(89deg, ${({ theme }) => theme.modal1} 0%, ${({ theme }) => theme.modal2} 100%);
+  }
+
+  & .cta-btn--modal {
+    &:hover {
+      background: linear-gradient(89deg, ${({ theme }) => theme.primary} 0%, ${({ theme }) => theme.secondary} 100%);
+    }
+  }
+
+  & .backgroundwhite {
+    background: linear-gradient(89deg, ${({ theme }) => theme.modal1} 0%, ${({ theme }) => theme.modal2} 100%);
+  }
+
+  & .Tilt {
+    background: linear-gradient(89deg, ${({ theme }) => theme.primary} 0%, ${({ theme }) => theme.tertiary} 100%);
+  }
+  
+  & .modal {
+    background-image: linear-gradient(89deg, ${({ theme }) => theme.modal1} 0%, ${({ theme }) => theme.modal2} 100%);
+  }
+
+  & .modal-body {    
+    text-shadow: 0 5px ${({ theme }) => theme.shadow};
+  }
+  `
