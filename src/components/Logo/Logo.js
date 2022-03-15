89f1c74e762ad6fd8054ba2aea3de61b8224540f
papstchaka <alex.christoph.ac@gmail.com>
2022-03-15 11:51:03 +0100

    added ScoreBoard

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import Tilt from 'react-tilt';
+import ScoreBoard from "../ScoreBoard/ScoreBoard";
 
 import brain from './brain.png'
 import './Logo.css'
 
-function Logo() {
+function Logo({ scoreboard }) {
     return (
-        <div className="ma4 mt0">
-            <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
+        <div className="ma4 mt0 mygrid">
+            <Tilt className="Tilt br2 shadow-2 logo" options={{ max : 55 }}>
                 <div className="Tilt-inner pa3" style={{textAlign: "center"}}>
                     <img src={brain} alt="logo" style={{paddingTop: "5px"}}/>
                 </div>
             </Tilt>
+            <ScoreBoard scoreboard={scoreboard}/>
         </div>
     );
 }
