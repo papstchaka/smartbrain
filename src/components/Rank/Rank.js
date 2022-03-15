1ff504d7eb396758d6090985da201147c5c1bb6e
papstchaka <alex.christoph.ac@gmail.com>
2022-03-15 14:04:37 +0100

    uppercased name

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -6,7 +6,7 @@ function Rank({name, entries}) {
     return (
         <div>
             <div className="f2 b center">
-                <p className='ma0 pbackground'>{`${name}, you already found...`}</p>
+                <p className='ma0 pbackground'>{`${name.toUpperCase()}, you already found...`}</p>
             </div>
             <div className="f2 b center">
                 <p className='ma0 pbackground'>{`${entries} faces!`}</p>
