9402c95374606b721469305faf18e960b994da3d
papstchaka <alex.christoph.ac@gmail.com>
2022-03-18 16:13:50 +0100

    switched from buttons to dropdown menu

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ThemeButton from "../Themes/ThemeButton";
 import _Modal from "./Modal";
+import { Dropdown } from 'react-bootstrap'
 
 import './Navigation.css';
 
@@ -21,13 +22,16 @@ function Navigation( {onRouteChange, isSignedIn, deleteProfile, triggerToggle, t
             <>
                 <nav style={{display: "flex", justifyContent: 'space-between'}}>
                     <ThemeButton triggerToggle={triggerToggle} toggle={toggle}/>
-                    <div>
-                        <p onClick={handleShowProfile} className='cta-btn cta-btn--hero sticksy'>
-                            Profile
-                        </p>
-                        <p onClick={handleSignout} className='cta-btn cta-btn--hero sticksy'>
-                            Sign Out
-                        </p>
+                    <div className="ma2 tc">
+                        <Dropdown>
+                            <Dropdown.Toggle variant="success"style={{background: "none", border: "none"}}>
+                                <img src="https://img.myloview.de/poster/house-icon-element-of-zoo-for-mobile-concept-and-web-apps-icon-outline-thin-line-icon-for-website-design-and-development-app-development-400-161676774.jpg" className="br-100 dib" alt="avatar" style={{width: "36px", height: "36px", backdropFilter: "blur(20px)"}}/>
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu style={{display: "flex", flexDirection: "column", zIndex: "999", borderRadius: "5px", border: "2px solid white"}}>
+                                <Dropdown.Item onClick={handleShowProfile}><p className='cta-btn cta-btn--hero sticksy'>Profile</p></Dropdown.Item>
+                                <Dropdown.Item onClick={handleSignout}><p className='cta-btn cta-btn--hero sticksy'>Sign Out</p></Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
                     </div>
                 </nav>
                 <_Modal handleCloseProfile={handleCloseProfile} handleShowProfile={handleShowProfile} showProfile={showProfile} deleteProfile={deleteProfile} user={user} loadUser={loadUser} getScoreBoard={getScoreBoard}/>
@@ -37,13 +41,16 @@ function Navigation( {onRouteChange, isSignedIn, deleteProfile, triggerToggle, t
         return (
             <nav style={{display: "flex", justifyContent: 'space-between'}}>
                 <ThemeButton triggerToggle={triggerToggle} toggle={toggle}/>
-                <div>
-                    <p onClick={() => onRouteChange('signin')} className='cta-btn cta-btn--hero sticksy'>
-                        Sign In
-                    </p>
-                    <p onClick={() => onRouteChange('register')} className='cta-btn cta-btn--hero sticksy'>
-                        Register
-                    </p>
+                <div className="ma2 tc">
+                    <Dropdown>
+                        <Dropdown.Toggle variant="success"style={{background: "none", border: "none"}}>
+                            <img src="https://img.myloview.de/poster/house-icon-element-of-zoo-for-mobile-concept-and-web-apps-icon-outline-thin-line-icon-for-website-design-and-development-app-development-400-161676774.jpg" className="br-100 dib" alt="avatar" style={{width: "36px", height: "36px", backdropFilter: "blur(20px)"}}/>
+                        </Dropdown.Toggle>
+                        <Dropdown.Menu style={{display: "flex", flexDirection: "column", zIndex: "999", borderRadius: "5px", border: "2px solid white"}}>
+                            <Dropdown.Item onClick={() => onRouteChange('signin')}><p className='cta-btn cta-btn--hero sticksy'>Sign In</p></Dropdown.Item>
+                            <Dropdown.Item onClick={() => onRouteChange('register')}><p className='cta-btn cta-btn--hero sticksy'>Register</p></Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
                 </div>
             </nav>
         );
