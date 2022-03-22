150ff609e687592c439c63d46dced066d2d7cee8
papstchaka <alex.christoph.ac@gmail.com>
2022-03-22 10:00:56 +0100

    reorganised style implementation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -22,10 +22,10 @@ function Navigation( {onRouteChange, isSignedIn, deleteProfile, triggerToggle, t
                     <ThemeButton triggerToggle={triggerToggle} toggle={toggle}/>
                     <div className="ma2 tc">
                         <Dropdown>
-                            <Dropdown.Toggle variant="success"style={{background: "none", border: "none", padding: "0"}}>
-                                <img src="https://img.myloview.de/poster/house-icon-element-of-zoo-for-mobile-concept-and-web-apps-icon-outline-thin-line-icon-for-website-design-and-development-app-development-400-161676774.jpg" className="br-100 dib" alt="avatar" style={{width: "36px", height: "36px", backdropFilter: "blur(20px)"}}/>
+                            <Dropdown.Toggle variant="success" className="dropdown-toggle">
+                                <img src="https://img.myloview.de/poster/house-icon-element-of-zoo-for-mobile-concept-and-web-apps-icon-outline-thin-line-icon-for-website-design-and-development-app-development-400-161676774.jpg" className="br-100 dib dropdown-img" alt="avatar"/>
                             </Dropdown.Toggle>
-                            <Dropdown.Menu style={{display: "flex", flexDirection: "column", zIndex: "999", borderRadius: "5px", border: "2px solid white"}}>
+                            <Dropdown.Menu className="dropdown-menu">
                                 <Dropdown.Item onClick={handleShowProfile}><p className='cta-btn cta-btn--hero sticksy'>Profile</p></Dropdown.Item>
                                 <Dropdown.Item onClick={handleSignout}><p className='cta-btn cta-btn--hero sticksy'>Sign Out</p></Dropdown.Item>
                             </Dropdown.Menu>
@@ -41,10 +41,10 @@ function Navigation( {onRouteChange, isSignedIn, deleteProfile, triggerToggle, t
                 <ThemeButton triggerToggle={triggerToggle} toggle={toggle}/>
                 <div className="ma2 tc">
                     <Dropdown>
-                        <Dropdown.Toggle variant="success"style={{background: "none", border: "none", padding: "0"}}>
-                            <img src="https://img.myloview.de/poster/house-icon-element-of-zoo-for-mobile-concept-and-web-apps-icon-outline-thin-line-icon-for-website-design-and-development-app-development-400-161676774.jpg" className="br-100 dib" alt="avatar" style={{width: "36px", height: "36px", backdropFilter: "blur(20px)"}}/>
+                        <Dropdown.Toggle variant="success" className="dropdown-toggle">
+                            <img src="https://img.myloview.de/poster/house-icon-element-of-zoo-for-mobile-concept-and-web-apps-icon-outline-thin-line-icon-for-website-design-and-development-app-development-400-161676774.jpg" className="br-100 dib dropdown-img" alt="avatar"/>
                         </Dropdown.Toggle>
-                        <Dropdown.Menu style={{display: "flex", flexDirection: "column", zIndex: "999", borderRadius: "5px", border: "2px solid white"}}>
+                        <Dropdown.Menu className="dropdown-menu">
                             <Dropdown.Item onClick={() => onRouteChange('signin')}><p className='cta-btn cta-btn--hero sticksy'>Sign In</p></Dropdown.Item>
                             <Dropdown.Item onClick={() => onRouteChange('register')}><p className='cta-btn cta-btn--hero sticksy'>Register</p></Dropdown.Item>
                         </Dropdown.Menu>
