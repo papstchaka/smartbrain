3bb76cd5f94b3762a04bc1f8267ee687914c65f7
papstchaka <alex.christoph.ac@gmail.com>
2022-03-26 13:34:13 +0100

    minor change in style implementation

--- src/components/ScoreBoard/ScoreBoard.js
+++ src/components/ScoreBoard/ScoreBoard.js
@@ -18,10 +18,10 @@ function ScoreBoard({ scoreboard }) {
     
     return (
             <div className="Tilt-inner pa2 scoreboard br2 shadow-1 gradient-border" style={useWindowSize()}>
-                <p className='mt0 mb1 myp'>Scoreboard: </p>
+                <p className='mt0 mb1'>Scoreboard: </p>
                 {
                     scoreboard.map((entries) => {
-                        return <p className='mb0 myp'>{entries.name}: {entries.count}</p>
+                        return <p className='mb0'>{entries.name}: {entries.count}</p>
                     })
                 }
             </div>
